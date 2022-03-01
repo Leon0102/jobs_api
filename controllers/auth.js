109bcf65d4e6b1b38120e5726814b36b561a186a
LeonNguyen <linhnlh2001@gmail.com>
2022-03-02 02:00:31 +0700

    first commit

--- controllers/auth.js
+++ controllers/auth.js
@@ -3,11 +3,10 @@ const {StatusCodes} = require('http-status-codes');
 const {BadRequestError} = require('../errors');
 
 const register = async (req, res, next) => {
-    const {name, email, password} = req.body;
-    if(!name || !email || !password) {
-        throw new BadRequestError('Missing required fields');
-    }
-
+    // const {name, email, password} = req.body;
+    // if(!name || !email || !password) {
+    //     throw new BadRequestError('Missing required fields');
+    // }
     const user = await User.create({...req.body});
     console.log({user});
     res.status(StatusCodes.CREATED).json({user});
